59b4a18b31dc916b610cd932d87996e4527372e1
sefhirot69 <josalillo@gmail.com>
2021-09-22 13:26:36 +0200

    feat : first commit

---
+++ src/HookApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+HookApp.propTypes = {
+
+};
+
+function HookApp(props) {
+    return (
+        <>
+            <h1>Hooks App</h1>
+        </>
+    );
+}
+
+export default HookApp;
