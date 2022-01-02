e80222115e6c490941e767f6bb26beb85b5a00eb
sefhirot69 <josalillo@gmail.com>
2022-01-02 17:10:27 +0100

    feat : useState coords

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Message = () => {
 
+    const [coords, setCoords] = useState({x: 0, y: 0});
+    const {x, y} = coords;
+
     useEffect(() => {
-        console.log('componente montado');
-        return() => {
-            console.log('componente desmontado');
+
+        const mouseMove = (e) => {
+            const coors = {x: e.x, y: e.y};
+            setCoords(coors);
+        };
+        window.addEventListener('mousemove', mouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove);
         }
-    },[]);
+    }, []);
 
     return (
         <>
             <h3>Eres imbécil</h3>
+            <p>
+                x: { x } y: { y }
+            </p>
         </>
     );
 };
