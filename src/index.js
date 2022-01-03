c98ba1d23618ce887277dd3af24542131d48f17f
sefhirot69 <josalillo@gmail.com>
2022-01-03 22:06:53 +0100

    feat : useRef

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 // import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
 // import SimpleForm from "./components/02-useEffect/SimpleForm";
 // import FormWithCustomHook from "./components/02-useEffect/FormWIthCustomHook";
-import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks";
+// import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks";
+import FocusScreen from "./components/04-useRef/FocusScreen";
 
 ReactDOM.render(
-    <MultipleCustomHooks />,
+    <FocusScreen />,
   document.getElementById('root')
 );
 
