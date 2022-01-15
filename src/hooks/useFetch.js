4f711b42158f4d79d1d959bb16022de29d538a1e
sefhirot69 <josalillo@gmail.com>
2022-01-15 17:12:32 +0100

    feat : tests sobre useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -28,6 +28,13 @@ const useFetch = (url) => {
                         data
                     })
                 }
+            })
+            .catch( () => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                });
             });
     }, [url]);
 
